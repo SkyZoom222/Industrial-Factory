d8c966989170c18421707120b8e24ca73be8629d
Nikita Belov <SkyZoom222@gmail.com>
2023-05-29 15:51:28 +0300

    Conveer fixed

--- Industrial Factory Monogame Project/Project1/DroppedItem.cs
+++ Industrial Factory Monogame Project/Project1/DroppedItem.cs
@@ -1,5 +1,5 @@
 ﻿using Microsoft.Xna.Framework;
-
+using System.Drawing.Drawing2D;
 
 namespace Industrial_Factory
 {
@@ -7,6 +7,7 @@ namespace Industrial_Factory
     {
         public int id, speed;
         public Rectangle Item;
+        Point ObjOffset = new Point(20);
 
         public DroppedItem(int id, int x, int y)
         {
@@ -20,20 +21,25 @@ namespace Industrial_Factory
             Rectangle HB = Item;
             switch (dir)
             {
-                case 0: // -- Up Direction
+                case 0: // -- Down Direction
                     HB = new Rectangle(Item.X, Item.Y + 3, Item.Width, Item.Height);
+                    ObjOffset = new Point(20, 0);
                     break;
                 case 1: // -- Left Direction
                     HB = new Rectangle(Item.X - 3, Item.Y, Item.Width, Item.Height);
+                    ObjOffset = new Point(40, 20);
                     break;
-                case 2: // -- Down Direction
+                case 2: // -- Up Direction
                     HB = new Rectangle(Item.X, Item.Y - 3, Item.Width, Item.Height);
+                    ObjOffset = new Point(20, 40);
                     break;
                 case 3: // -- Right Direction
                     HB = new Rectangle(Item.X + 3, Item.Y, Item.Width, Item.Height);
+                    ObjOffset = new Point(0, 20);
                     break;
             }
-            foreach (var item in Data.droppedItems) if (Item != item.Item && HB.Intersects(item.Item))
+            foreach (var item in Data.droppedItems) 
+                if (Item != item.Item && HB.Intersects(item.Item))
                 {
                     return;
                 }
@@ -43,15 +49,19 @@ namespace Industrial_Factory
             if (dir == 3) Item.X += speed;
         }
 
-        public void OnConveyorCheck() //Obj, (int)Data.ObjInf[x, y][2]
+        public void OnConveyorCheck() //Grid, (int)Data.ObjInf[x, y][2]
         {
             int scale = (int)(Data.SizeObj * Data.ObjScale); //scale for Drawing obj
              
-            Point Obj = new Point((Item.X + 20 )/ scale, (Item.Y + 20) / scale);
-            if (Data.ObjMap[Obj.X, Obj.Y] != null && Data.ObjMap[Obj.X, Obj.Y].Tag == "conveer")
+            Point Grid = new Point((Item.X + ObjOffset.X)/ scale, (Item.Y + ObjOffset.Y) / scale);
+            if (Data.ObjMap[Grid.X, Grid.Y] != null && Data.ObjMap[Grid.X, Grid.Y].Tag == "conveer")
             {
-                Move((int)Data.ObjInf[Obj.X, Obj.Y][2]);
+                Move((int)Data.ObjInf[Grid.X, Grid.Y][2]);
             }
+
+
+
+
         }
 
     }
